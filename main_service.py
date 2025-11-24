66da467a982f6abffacc86d0fc8c3d9d11a374bc
Pavel Salavei <pavel.salavei.ext@artus-gruppe.com>
2025-11-24 17:15:18 +0300

    Lifespan fix

--- main_service.py
+++ main_service.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI
-from contextlib import asynccontextmanager
+from contextlib import asynccontextmanager, _AsyncGeneratorContextManager
+
 from configs.cors_config import add_cors_middleware
 from configs.create_tables import create_tables
 
@@ -10,33 +11,38 @@ from routers.protected import router as protected_router
 from routers.reset import router as reset_router
 
 class LoginMainService:
-    def __init__(self):
-        self.app = FastAPI(
-            title="Login Project",
-            description="Login Project is about signup, login, JWT",
-            version="1.0.0"
-        )
-
-    def configure_cors(self):
-        add_cors_middleware(self.app)
-
-    def configure_routers(self):
-        self.app.include_router(auth_router)
-        self.app.include_router(protected_router)
-        self.app.include_router(reset_router)
-
-    def configure_lifespan(self):
+    def _configure_lifespan(self) -> _AsyncGeneratorContextManager[None, None]:
         @asynccontextmanager # FastAPI expects lifespan to be async context manager. A context manager is an object you can use with 'async with' or 'with' that automatically handles setup and cleanup around a block of code.
         async def lifespan(app: FastAPI):
             logger.info("Server starting up...")
             await create_tables()
-            yield
-            logger.info("Server shutting down...")
 
-        self.app.router.lifespan_context = lifespan # No need to call
+            try:
+                yield
+            finally:
+                logger.info("Server shutting down...")
+            
+        return lifespan
+        
+    def __init__(self):
+        self.lifespan = self._configure_lifespan()
 
-    def run(self):
-        self.configure_cors()
-        self.configure_routers()
-        self.configure_lifespan()
+        self.app = FastAPI(
+            title="Login Project",
+            description="Login Project is about signup, login, JWT",
+            version="1.0.0",
+            lifespan=self.lifespan
+        )
+
+    def _configure_cors(self) -> None:
+        add_cors_middleware(self.app)
+
+    def _configure_routers(self) -> None:
+        self.app.include_router(auth_router)
+        self.app.include_router(protected_router)
+        self.app.include_router(reset_router)
+
+    def run(self) -> FastAPI:
+        self._configure_cors()
+        self._configure_routers()
         return self.app
